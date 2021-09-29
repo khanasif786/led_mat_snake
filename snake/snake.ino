666ebb209fa36331a0f8e172bfd7b8b81b91c914
Asif Khan <asif.kgauri@gmail.com>
2021-09-29 18:23:35 +0530

    completed snake code with 4 buttons  -> added length slgo to all axis  -> added proper random point  -> completed perfect light delays

--- snake/snake.ino
+++ snake/snake.ino
@@ -14,8 +14,13 @@ unsigned long current_time = 0 ;
 unsigned long previous_time = 0 ;
 unsigned long time_interval = 0 ;
 
-int random_pos_x = 4 ;
-int random_pos_y = 4 ;
+int pos_x_array[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int pos_y_array[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int snake_length = 1 ;
+
+int random_pos_x = 0 ; // = 4 ;
+int random_pos_y = 0 ; // = 4 ;
 
 void setup() {
   int i ;
@@ -23,10 +28,10 @@ void setup() {
     pinMode(cathode[i], OUTPUT);
     pinMode(anode[i], OUTPUT);
   }
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
 
   resetAnode();
   resetCathode();
@@ -39,11 +44,12 @@ void loop() {
 
 ////////////////////////////FUNCTIONS////////////////////////
 void go_right() {
-
+  int i = 0 ;
   while (current_pos_x <= 7) {
 
     if((current_pos_x == random_pos_x) && (current_pos_y ==  random_pos_y)){
       GenerateRandom();
+      snake_length++ ; 
     }
     lit_random();
     
@@ -56,26 +62,40 @@ void go_right() {
         go_down();
       }
     }
-    lit(current_pos_x, current_pos_y); 
+    
+    for(i=0;i<snake_length;i++){
+    lit(pos_x_array[i], pos_y_array[i]);
+    } 
     
     current_time = millis() ;
     time_interval = current_time - previous_time ;
+    
     if(time_interval>=300){
     current_pos_x++;
     previous_time = current_time ;
-    }
-    
+
     if (current_pos_x == 8) {
       current_pos_x = 0;
     }
+    
+    for(i=(snake_length-1);i>0;i--){
+      pos_x_array[i] = pos_x_array[i-1];
+      pos_y_array[i] = pos_y_array[i-1];
+    }
+    pos_x_array[0] = current_pos_x ;
+    pos_y_array[0] = current_pos_y ;
+    }
+    
+
   }
 }
 void go_left() {
-
+  int i = 0  ;
   while (current_pos_x >= 0) {
 
     if((current_pos_x == random_pos_x) && (current_pos_y ==  random_pos_y)){
       GenerateRandom();
+      snake_length++ ;
     }
     lit_random();
     
@@ -88,26 +108,36 @@ void go_left() {
         go_down();
       }
     }
-    lit(current_pos_x, current_pos_y);
+    for(i=0;i<snake_length;i++){
+    lit(pos_x_array[i], pos_y_array[i]);
+    } 
     
     current_time = millis() ;
     time_interval = current_time - previous_time ;
     if(time_interval>=300){
     current_pos_x--;
     previous_time = current_time ;
-    }
-    
     if (current_pos_x == -1) {
       current_pos_x = 7;
     }
+    for(i=(snake_length-1);i>0;i--){
+      pos_x_array[i] = pos_x_array[i-1];
+      pos_y_array[i] = pos_y_array[i-1];
+    }
+    pos_x_array[0] = current_pos_x ;
+    pos_y_array[0] = current_pos_y ;
+    }
+    
+
   }
 }
 void go_down() {
-
+  int i = 0 ;
   while (current_pos_y <= 7) {
     
     if((current_pos_x == random_pos_x) && (current_pos_y ==  random_pos_y)){
       GenerateRandom();
+      snake_length++ ;
     }
     lit_random();
     
@@ -118,23 +148,34 @@ void go_down() {
       }
       if (CurrentState == 'l') {
         go_left();
-      }
+      } 
     }
-    lit(current_pos_x, current_pos_y);
+
+    for(i=0;i<snake_length;i++){
+    lit(pos_x_array[i], pos_y_array[i]);
+    } 
     current_time = millis() ;
     time_interval = current_time - previous_time ;
     if(time_interval>=300){
     current_pos_y++;
     previous_time = current_time ;
-    }
+    
     if (current_pos_y == 8) {
       current_pos_y = 0;
     }
+    for(i=(snake_length-1);i>0;i--){
+      pos_x_array[i] = pos_x_array[i-1];
+      pos_y_array[i] = pos_y_array[i-1];
+    }
+    pos_x_array[0] = current_pos_x ;
+    pos_y_array[0] = current_pos_y ;
+    }
+
 
   }
 }
 void go_up() {
-
+  int i = 0 ;
   while (current_pos_y >= 0) {
     if((current_pos_x == random_pos_x) && (current_pos_y ==  random_pos_y)){
       GenerateRandom();
@@ -150,18 +191,28 @@ void go_up() {
         go_left();
       }
     }
-    lit(current_pos_x, current_pos_y);
+    for(i=0;i<snake_length;i++){
+    lit(pos_x_array[i], pos_y_array[i]);
+    } 
 
     current_time = millis() ;
     time_interval = current_time - previous_time ;
     if(time_interval>=300){
     current_pos_y--;
     previous_time = current_time ;
-    }
-
+    
     if (current_pos_y == -1) {
       current_pos_y = 7;
     }
+    for(i=(snake_length-1);i>0;i--){
+      pos_x_array[i] = pos_x_array[i-1];
+      pos_y_array[i] = pos_y_array[i-1];
+    }
+    pos_x_array[0] = current_pos_x ;
+    pos_y_array[0] = current_pos_y ;
+    }
+
+
   }
 }
 
@@ -170,6 +221,7 @@ void lit(int cathodePin, int anodePin) {
   resetAnode();
   digitalWrite(cathode[cathodePin], 1);
   digitalWrite(anode[anodePin], 0);
+  delay(3);
 }
 
 void resetCathode() {
